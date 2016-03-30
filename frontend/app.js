991576de3905de5fb6e20f0efe7ed87942ea2eda
Dan Richards <dan.richards@lush.co.uk>
2016-03-30 17:59:56 +0100

    Updated app.js to load adaro and setup some example routes.
    Added mock posts file to use as example content.
    
    Added articles and index dust templates.

--- frontend/app.js
+++ frontend/app.js
@@ -4,14 +4,43 @@
  */
 
 var express = require('express');
+var adaro = require('adaro');
+
+// Setup the app variables.
 var app = express();
 var hostname = '192.168.56.111';
 var port = process.env.PORT || 3000;
 
-app.get('/example', function (req, res) {
-  res.send('Hello world.');
+// Setup adaro.
+app.set('views', 'templates');
+app.engine('dust', adaro.dust({cache: false}));
+app.set('view engine', 'dust');
+
+// Example model.
+var model = {
+  name: 'my-name',
+  location: 'my-location',
+  vice: 'Coffee'
+};
+
+// Example content.
+var posts = require('./mock/posts.js');
+
+// Home route.
+app.get('/', function (req, res) {
+  res.render('index', model);
 });
 
-app.listen(port, hostname);
+// API route.
+app.get('/api', function (req, res) {
+  res.send(model);
+});
 
+// Articles route.
+app.get('/articles', function (req, res) {
+  res.render('articles', posts);
+});
+
+// Get things going.
+app.listen(port, hostname);
 console.log('App is listening on http://%s:%s', hostname, port);
---
+++ frontend/mock/posts.js
@@ -0,0 +1,22 @@
+/**
+ * @file
+ * Mock posts content for the 'Headless Training' front end.
+ */
+
+module.exports = {
+  name: 'Matthew',
+  posts: [
+    {
+      'id': 1,
+      'title': 'First Post!',
+      'body': 'This is the first entry in my headless drupal blog.',
+      'published': '9/20/2015'
+    },
+    {
+      'id': 2,
+      'title': 'Headless Drupal is the way forward.',
+      'body': 'This is the second entry in my headless drupal blog.',
+      'published': '9/27/2015'
+    }
+  ]
+};
