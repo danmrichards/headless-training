5afd4243a067fadda58b2ac68d7edc532d9cc128
Dan Richards <dan.richards@lush.co.uk>
2016-03-31 18:25:06 +0100

    Added new template for blog route Refactored /blog route to pass data to the template Refactored cache key generation to mitigate conflicts Started building individual post route

--- frontend/app.js
+++ frontend/app.js
@@ -23,16 +23,48 @@ var port = process.env.PORT || 3000;
 var api = 'http://headless-training.dev/api';
 var blogEndpoint = api + '/v1.1/blogs';
 var cacheKey;
+var posts;
 
 // Setup adaro.
 app.set('views', 'templates');
 app.engine('dust', adaro.dust({cache: false}));
 app.set('view engine', 'dust');
 
-// Set up the blog route.
+// Blogs route.
 app.get('/blog', function (req, res) {
   // Build the cache key.
-  cacheKey = crypto.createHash('sha1').update(req.url + port).toString();
+  cacheKey = crypto.createHash('sha1').update(req.path).digest('hex');
+
+  // Check if a cached version of this data already exists.
+  client.mget(cacheKey, function (err, results) {
+    // We have some cached data so use it.
+    if (results[0] !== null) {
+      posts = JSON.parse(results);
+      return res.render('posts', posts);
+    }
+    else {
+      // The cache was empty so pull it from Drupal.
+      request({
+        url: blogEndpoint,
+        json: true
+      }, function (error, response, body) {
+        // Update the cache and show the response.
+        client.multi([
+          ['set', cacheKey, JSON.stringify(body)],
+          ['expire', cacheKey, 800]
+        ]).exec(function (err, replies) {
+          posts = body;
+          return res.render('posts', posts);
+        });
+      });
+    }
+  });
+});
+
+// Blog post route.
+app.get('/blog/:id', function (req, res) {
+  // Build the cache key.
+  cacheKey = crypto.createHash('sha1').update(req.path).digest('hex');
 
   // Check if a cached version of this data already exists.
   client.mget(cacheKey, function (err, results) {
@@ -43,7 +75,7 @@ app.get('/blog', function (req, res) {
     else {
       // The cache was empty so pull it from Drupal.
       request({
-        url: blogEndpoint,
+        url: blogEndpoint + '/' + req.params.id,
         json: true
       }, function (error, response, body) {
         // Update the cache and show the response.
