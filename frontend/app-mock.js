e3ceb7780a498e4d223a863ed12fabde723ea594
Dan Richards <dan.richards@lush.co.uk>
2016-03-31 13:24:54 +0100

    Moved mock templates into a mocks sub direcotry Updated the mock app js to point at the new template directory

--- frontend/app-mock.js
+++ frontend/app-mock.js
@@ -12,7 +12,7 @@ var hostname = '192.168.56.111';
 var port = process.env.PORT || 3000;
 
 // Setup adaro.
-app.set('views', 'templates');
+app.set('views', 'templates/mocks');
 app.engine('dust', adaro.dust({cache: false}));
 app.set('view engine', 'dust');
 
